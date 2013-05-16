93fbb230a1291b0f78a50b286f2b756e6854fda1
yurak <yurak.sisa.dream@gmail.com>
2013-05-16 16:13:08 +0200

    error on fieldset render + new content type creation

--- lib/ysd_plugin_event_extension.rb
+++ lib/ysd_plugin_event_extension.rb
@@ -1,5 +1,7 @@
 require 'ysd-plugins_viewlistener' unless defined?Plugins::ViewListener
 require 'ysd_md_fieldset_event'
+require 'ysd_md_cms' unless defined?ContentManagerSystem::Content
+require 'ysd-md-profile' unless defined?Users::Group
 
 #
 # Event aspect Extension
@@ -8,6 +10,40 @@ module Huasi
 
   class EventExtension < Plugins::ViewListener
     include ContentManagerSystem::Support
+
+    # ========= Installation =================
+
+    # 
+    # Install the plugin
+    #
+    def install(context={})
+
+      ContentManagerSystem::ContentType.first_or_create({:id => 'event'},
+        {:name => 'Evento', 
+         :description => 'Eventos',
+         :publishing_workflow_id => 'standard',
+         :usergroups => [Users::Group.get('staff')]})
+
+      ContentManagerSystem::View.first_or_create({:view_name => 'events_page'},
+       {
+         :description => 'Published events page',
+         :model_name => 'content',
+         :query_conditions => {:field => 'type', :operator => '$eq', :value => 'event'},
+         :query_order => [{:field => 'event_date_start', :order => :desc}],
+         :query_arguments => [],
+         :style => :teaser,
+         :v_fields => [],
+         :render => :teaser,
+         :render_options => {},
+         :view_limit => 0,
+         :pagination => true,
+         :ajax_pagination => false,
+         :page_size => 25,
+         :pager => :page_list,
+         :block => false
+       })
+    
+    end
 
     # ========= Aspects ==================
     
--- ysd_plugin_event.gemspec
+++ ysd_plugin_event.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name    = "ysd_plugin_event"
-  s.version = "0.1.1"
+  s.version = "0.1.3"
   s.authors = ["Yurak Sisa Dream"]
   s.date    = "2012-03-16"
   s.email   = ["yurak.sisa.dream@gmail.com"]
@@ -9,7 +9,10 @@ Gem::Specification.new do |s|
   s.summary = "CMS extension to manage events"
   
   s.add_runtime_dependency "ysd_core_plugins"
-  s.add_runtime_dependency "ysd_md_fieldset"  
+  s.add_runtime_dependency "ysd_md_fieldset" 
+  s.add_runtime_dependency "ysd_md_cms"
+  s.add_runtime_dependency "ysd_md_profile"
+
   s.add_runtime_dependency "ysd_yito_core"
   s.add_runtime_dependency "ysd_yito_js"
 
